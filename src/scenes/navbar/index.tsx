ad0fc58871590c6d5c8ddd01256431b248afea4a
Anthony Arinze <anthonyarinze28@gmail.com>
2023-03-11 23:48:25 +0100

    Navbar

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,14 +1,34 @@
-import React from 'react'
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import Logo from "@/assets/Logo.png";
 
-
-
-type Props = {}
+type Props = {};
 
 const Navbar = (props: Props) => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const flexBetween = "flex items-center justify-between";
 
-export default Navbar
+  return (
+    <nav>
+      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+        <div className={`${flexBetween} mx-auto w-5/6`}>
+          <div className={`${flexBetween} w-full gap-16`}>
+            {/* LEFT SIDE */}
+            <img alt="logo" src={Logo} />
+
+            {/* RIGHT SIDE */}
+            <div className={`${flexBetween} w-full`}>
+              {/* LINKS */}
+              <div></div>
+
+              {/* SIGN IN & MEMBERSHIP */}
+              <div></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
