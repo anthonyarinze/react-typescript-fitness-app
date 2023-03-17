29af0a12bf8eb80fa46217a6220e5e20f20e9307
Anthony Arinze <anthonyarinze28@gmail.com>
2023-03-17 15:13:16 +0100

    Classes Page

---
+++ src/scenes/contactUs/index.tsx
@@ -0,0 +1,12 @@
+import { SelectedPage } from "@/shared/types";
+import React from "react";
+
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const ContactUs = ({ setSelectedPage }: Props) => {
+  return <div>ContactUs</div>;
+};
+
+export default ContactUs;
